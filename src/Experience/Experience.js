f7f6bb3cd858772b813d7d8fa1189fd2ae781e36
Daria Borisiak <daria.borisiak@gmail.com>
2026-04-27 22:38:35 +0200

    Set up camera

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -2,6 +2,10 @@ import * as THREE from 'three'
 import Resources from './Utils/Resources.js'
 
 import sources from './sources.js'
+import Debug from './Utils/Debug.js'
+import Sizes from './Utils/Sizes.js'
+import Time from './Utils/Time.js'
+import Camera from './Camera.js'
 
 let instance = null
 
@@ -21,7 +25,61 @@ export default class Experience
         this.canvas = _canvas
 
         // Setup
+        this.debug = new Debug()
+        this.sizes = new Sizes()
+        this.time = new Time()
         this.scene = new THREE.Scene()
         this.resources = new Resources(sources)
+        this.camera = new Camera()
+
+        // Resize event
+        this.sizes.on('resize', () => {
+            this.resize()
+        })
+
+        // Time tick event
+        this.time.on('tick', () => {
+            this.update()
+        })
+    }
+
+    resize() {
+        this.camera.resize()
+    }
+
+    update() {
+        this.camera.update()
+    }
+
+    destroy() {
+        this.sizes.off('resize')
+        this.time.off('tick')
+
+        // Traverse the whole scene
+        this.scene.traverse((child) =>
+        {
+            // Test if it's a mesh
+            if(child instanceof THREE.Mesh)
+            {
+                child.geometry.dispose()
+
+                // Loop through the material properties
+                for(const key in child.material)
+                {
+                    const value = child.material[key]
+
+                    // Test if there is a dispose function
+                    if(value && typeof value.dispose === 'function')
+                    {
+                        value.dispose()
+                    }
+                }
+            }
+        })
+
+        this.camera.controls.dispose()
+
+        if (this.debug.active)
+            this.debug.ui.destroy()
     }
 }
